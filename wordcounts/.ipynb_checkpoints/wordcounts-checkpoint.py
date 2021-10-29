44ab1c484c4690dd47b79c6fcc08cc79960fbb53
Paul C. Owe <40435982+owecodes@users.noreply.github.com>
2021-10-28 22:54:19 -0400

    wordnet

--- wordcounts/.ipynb_checkpoints/wordcounts-checkpoint.py
+++ wordcounts/.ipynb_checkpoints/wordcounts-checkpoint.py
@@ -4,13 +4,14 @@ def wordcount_fn(file_uri):
     nparr_words = open(file_uri, 'r')
     text_as_str = nparr_words.read() #for small textfiles
     text_as_list = text_as_str.split()
-#     print(text_as_list)
+
     
     counts = nltk.FreqDist(text_as_list).items()
     for key, val in counts:
         print(str(val) + " " + str(key))
     return
-#     return
+
+
 print(f"Word counts for your file: {wordcount_fn('./textfile.txt')}")
     
         
